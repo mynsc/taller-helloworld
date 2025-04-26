3343920c4e4ae0d9857b1c970ac102f4c106f2b1
mynsc <mcerquin.10@gmail.com>
2025-04-25 21:48:13 -0500

    Ejercicio 2 completo

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void imprimir_z(int n){
+void imprimir_nw(int n) {
     for (int f = 0; f < n; f++) {
-        for (int c = 0; c < n; c++){
-            if (f == (n - 1) / 2){
+        for (int c = 0; c < n; c++) {
+            if (f == 0) {
+                if (c == 0 || c >= (n - 1) / 2){
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
+            } else if (f < (n - 1) / 2) {
+                if (c == 0 || c == (n - 1) / 2) {
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
+            } else if (f == (n - 1) / 2) {
                 cout << "*";
+            } else if (f > (n - 1) / 2 && f != n - 1) {
+                if (c == (n - 1) / 2 || c == n - 1) {
+                    cout  << "*";
+                } else {
+                    cout << " ";
+                }
             } else {
-                cout << "o";
+                if (c == n - 1 || c <= (n - 1) / 2) {
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
             }
         }
         cout << endl;
@@ -15,11 +37,11 @@ void imprimir_z(int n){
 }
 int main() {
     int n;
-    cout << "Ingrese un número impar y mayor igual que 5: ";
+    cout << "Ingrese un número impar que sea mayor o igual a 5: ";
     cin >> n;
 
     if (n % 2 == 1 && n>=5) {
-       imprimir_z(n);
+       imprimir_nw(n);
     } else {
         cout << "El número ingresado no es válido." << endl;
     }
