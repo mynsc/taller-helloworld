4d0f08656971ee351a2d8aa02148d774931dd608
mynsc <mcerquin.10@gmail.com>
2025-04-25 20:43:20 -0500

    Ejercicio 1 completo y ejercicio 2

---
+++ ejercicio1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void imprimir_z(int n){
+    for (int f = 0; f < n; f++) {
+        for (int c = 0; c < n; c++){
+            if (f == 0 || f == n - 1){
+                cout << "*";
+            } else if (f == n - c - 1){
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }    
+}
+int main() {
+    int n;
+    cout << "Ingrese un número impar y mayor igual que 3: ";
+    cin >> n;
+
+    if (n % 2 == 1 && n>=3) {
+       imprimir_z(n);
+    } else {
+        cout << "El número ingresado no es válido." << endl;
+    }
+    return 0;
+}
