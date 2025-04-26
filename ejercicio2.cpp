4a640001165fa7692937c3a7c6f286f0c79049af
mynsc <mcerquin.10@gmail.com>
2025-04-25 20:52:22 -0500

    Ejercicio 2 en el pasado

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void imprimir_z(int n){
+    for (int f = 0; f < n; f++) {
+        for (int c = 0; c < n; c++){
+            if (f == (n - 1) / 2){
+                cout << "*";
+            } else {
+                cout << "o";
+            }
+        }
+        cout << endl;
+    }    
+}
 int main() {
-    cout<<"hola"<<endl;
+    int n;
+    cout << "Ingrese un número impar y mayor igual que 5: ";
+    cin >> n;
+
+    if (n % 2 == 1 && n>=5) {
+       imprimir_z(n);
+    } else {
+        cout << "El número ingresado no es válido." << endl;
+    }
+    return 0;
 }
