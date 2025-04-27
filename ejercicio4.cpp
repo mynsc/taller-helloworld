614578cb8cf9577fa0876c18d62438ab9f3c3e42
mynsc <mcerquin.06.a@gmail.com>
2025-04-27 12:01:10 -0500

    Ejercicio 4 completado

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -3,36 +3,35 @@ using namespace std;
 
 void imprimir_TT(int n){
     for (int f = 0; f < n; f++) {
-        if (f == 0) {
-            for (int c = 0; c < n; c++) {
-                if (c <= (n - 1) / 2 || c == n - 1) {
+        for (int c = 0; c < n; c++){
+            if (f == 0) {
+                if (( c <= (n - 1) / 2) || c == n - 1) {
+                    cout << "*";
+                } else {
+                   cout << " ";
+                }
+            } else if (f == (n - 1) / 2) {
+                cout << "*";
+            } else if (f == n -1) {
+                if (c == 0 || c >= (n - 1) / 2) {
                     cout << "*";
                 } else {
                     cout << " ";
                 }
-            }
-            cout << endl;
-        }   else if ( f == c || c == (n - 1) / 2 || f == n - c - 1 || c == n - 1) {
-            for (int c = 0; c < n; c++) {
-                if ( f == c || c == (n - 1) / 2 || f == n - c - 1 || c == n - 1) {
-                if (( f == c || c == (n - 1) / 2 || f == n - c - 1 || c == n - 1) && (f < (n - 1) / 2)) {
-                    cout << "x";
-                } else {
-                    cout << "o";
-                }
-            }
-        }
-        }  if (f == (n - 1) / 2) {
-            cout << "*";
-        } else if (f == n - 1) {
-            for (int c = 0; c < n; c++) {
-                if (c >= (n - 1) / 2 || c == 0) {
+            } else if (f < (n - 1) / 2) {
+                if (c == f || c == (n - 1) / 2 || c == n - 1 - f || c == n - 1) {
                     cout << "*";
                 } else {
                     cout << " ";
                 }
-            } 
-         }
+            } else if (f > (n - 1) / 2) {
+                if (c == f || c == (n - 1) / 2 || c == n - 1 - f || c == 0) {
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
+            }    
+        }    
         cout << endl;
     }    
 }
